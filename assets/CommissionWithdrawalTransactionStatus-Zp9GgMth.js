77c5d5bdf587da50e14066a7ba89e8883633d7a1
NGUYEN PHU NHO <39373872+nguyenphunho@users.noreply.github.com>
2025-05-24 16:10:13 +0700

    Create CommissionWithdrawalTransactionStatus-Zp9GgMth.js
    CommissionWithdrawalTransactionStatus-Zp9GgMth.js

---
+++ assets/CommissionWithdrawalTransactionStatus-Zp9GgMth.js
@@ -0,0 +1,1 @@
+const e={Pending:1,Approved:2,Rejected:3},n=t=>{switch(t){case e.Pending:return"Pending";case e.Approved:return"Approved";case e.Rejected:return"Rejected";default:return"Unknown"}};export{n as g};
