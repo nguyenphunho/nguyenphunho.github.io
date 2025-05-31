95ca4a319214f10be6600554552964a33923e370
ENS AIRDROP <80233512+miniapp2025@users.noreply.github.com>
2025-05-31 16:29:56 +0700

    .
    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aga-goldfun/',
+  base: '/aga-goldfun.html/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
